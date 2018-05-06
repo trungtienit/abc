78dcb99b41765b838d5ef7d34f5bdf024e140007
Tran Tien <trungtien.uit@gmail.com>
2018-05-06 22:20:56 +0700

    add

--- test.php
+++ test.php
@@ -15,21 +15,10 @@
 	</head>
 	<body>
 <?php
-$headers = apache_request_headers();
+$headers = apache_request_headers();        
 foreach ($headers as $header => $value) {
-	if ($header == 'x-up-calling-line-id') {
-		$phone_number = $value;
-		break;
-	}
-}
-if (isset($phone_number)) {
-	echo "<p>Your phone number is:</p><h1>$value</h1><p>Did you know that this number is sent to <strong>every</strong> website you visit (when not using Wi-Fi)?</p>";
-} else {
-	echo '<p>No phone number found. Make sure Wi-Fi is turned off and <a href="./">try again</a>.</p>';
+ echo "<pre>";
+ echo "$header : $value";
+ echo "</pre>";
 }
 ?>
-		<p><a href="http://www.thinkbroadband.com/news/4990-o2-shares-your-mobile-phone-number-with-every-website-you-visit.html">More info here</a>.</p>
-		<p>Try <a href="http://lew.io/headers.php">this demo</a> by <a href="http://twitter.com/lewispeckover">@lewispeckover</a>.</p>
-		<p><a href="https://gist.github.com/1675764">Source code for this page</a>.</p>
-	</body>
-</html>
